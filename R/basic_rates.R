00575d73c64d2c3856349d7528808f9e7e6c9aa1
Erik_MS <78145948+Erik-Martinez@users.noreply.github.com>
2024-10-17 15:13:30 +0200

    Frecuency Rate completed

--- R/basic_rates.R
+++ R/basic_rates.R
@@ -1,11 +1,27 @@
+#' Basic Rate
+#'
+#'This function is the internal use to calculate some rates.
+#'
+#' @param num A number or a vector
+#' @param den A number or a vector
+#' @param factor A number
+#'
+#' @return Numeric
+#' @keywords internal
+
+basic_rate <- function(num, den, factor){
+  result <- (sum(num) * factor) / sum(den)
+  return(result)
+}
+
 #' Frequency rate
 #'
 #'A frequency rate indicate the number of new cases of injury
 #'occurring in relation to the amount of time during which workers in the reference group
 #'were "exposed to the risk" of being involved in an occupational accident.
-#' @param hours - Number total of Hours or vector
-#' @param acci - Number total of accidents or vector
-#' @param factor -
+#' @param hours Number total of Hours or vector
+#' @param acci Number total of accidents or vector
+#' @param factor Multipliavate factor
 #'
 #' @return numeric
 #' @export
@@ -13,7 +29,12 @@
 #' @examples
 #' frequency_rate(hours = 800000, acci = 80, factor = 10^6)
 frequency_rate <- function(hours, acci, factor = 10^6){
-  result <- (sum(acci) * factor) / sum(hours)
-  return(result)
+  result <- basic_rate(acci, hours, factor)
+  if (interactive()) {
+    cat(sprintf("Frequency Rate: %.2f per %d work hours in the reference group",
+                result, factor))
+  }
+
+  return(invisible(result))
 }
 
