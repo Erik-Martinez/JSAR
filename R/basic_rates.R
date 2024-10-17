96ab6a17b4d309f6f1a415f0fd525dbfcfd7424e
Erik_MS <78145948+Erik-Martinez@users.noreply.github.com>
2024-10-17 16:05:07 +0200

    Severity Rate addition

--- R/basic_rates.R
+++ R/basic_rates.R
@@ -19,19 +19,50 @@ basic_rate <- function(num, den, factor){
 #'A frequency rate indicate the number of new cases of injury
 #'occurring in relation to the amount of time during which workers in the reference group
 #'were "exposed to the risk" of being involved in an occupational accident.
-#' @param hours Number total of Hours or vector
-#' @param acci Number total of accidents or vector
-#' @param factor Multipliavate factor
 #'
-#' @return numeric
+#' @param hours Number of hours worked by workers in the reference group during
+#'  the reference period, or a vector to sum.
+#' @param acci Number of new cases of occupational injury during the reference period,
+#'  or a vector to sum.
+#' @param factor Multiplicative factor to adjust the rate (default is 10^6).
+#'
+#' @return Numeric value of the frequency rate
 #' @export
 #'
 #' @examples
-#' frequency_rate(hours = 800000, acci = 80, factor = 10^6)
+#' frequency_rate(hours = 800000, acci = 80)
 frequency_rate <- function(hours, acci, factor = 10^6){
   result <- basic_rate(acci, hours, factor)
   if (interactive()) {
-    cat(sprintf("Frequency Rate: %.2f per %d work hours in the reference group",
+    cat(sprintf("Frequency Rate: %.2f accidents per %d work hours",
+                result, factor))
+  }
+
+  return(invisible(result))
+}
+
+#' Severity Rate
+#'
+#' A severity rates mesure the time lost due to injures in relation to the total
+#' amount of time owrked.
+#'
+#' @param hours Number of hours worked by workers in the reference group during
+#'  the reference period or a vector to sum.
+#' @param days Number of days lost as a result of new cases of occupational
+#'  injury during the reference period, or a vector to sum. In case of permanent
+#'  incapacity each country have a scale to know how many days have to add for
+#'  this kind of workers.
+#' @param factor Multiplicative factor to adjust the rate (default is 10^3).
+#'
+#' @return Numeric value of the severity rate
+#' @export
+#'
+#' @examples
+#' severity_rate(hours = 800000, days = 300)
+severity_rate <- function(hours, days, factor = 10^3){
+  result <- basic_rate(days, hours, factor)
+  if (interactive()) {
+    cat(sprintf("Severity Rate: %.2f number of work days lost due to ocupaional injuries per %d work hours",
                 result, factor))
   }
 
--- tests/testthat/test-basic_rates.R
+++ tests/testthat/test-basic_rates.R
@@ -29,3 +29,37 @@ test_that("frecuency rates works with defaul factor", {
 
   expect_equal(result, expected_result)
 })
+
+#---------
+
+test_that("severity rates works with scalar values", {
+  hours <- 800000
+  days <- 300
+  factor <- 10^3
+
+  expected_result <- (days * factor) / hours
+  result <- severity_rate(hours = hours, days = days, factor = factor)
+
+  expect_equal(result, expected_result)
+})
+
+test_that("severity rates works with vector inputs", {
+  hours <- c(700000, 100000)
+  days <- c(150, 150)
+  factor <- 10^3
+
+  expected_result <- (sum(days) * factor) / sum(hours)
+  result <- severity_rate(hours = hours, days = days, factor = factor)
+
+  expect_equal(result, expected_result)
+})
+
+test_that("severity rates works with defaul factor", {
+  hours <- 800000
+  days <- 300
+
+  expected_result <- (sum(days) * 10^3) / sum(hours)
+  result <- severity_rate(hours = hours, days = days)
+
+  expect_equal(result, expected_result)
+})
